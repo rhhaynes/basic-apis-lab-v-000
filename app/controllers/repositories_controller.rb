9db1946812dd61d26ff1282fc2484c9620afdbbd
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-07 13:37:26 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,7 +8,7 @@ class RepositoriesController < ApplicationController
       @resp = Faraday.get "https://api.github.com/search/repositories?q=#{params[:query]}"
       binding.pry
     rescue Faraday::Timeout
-      @error = ""
+      @error = "Timeout error. Please try again."
     end
     render 'search'
   end
