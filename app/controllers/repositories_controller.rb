c197aa7bb71df84b377c0b679165adb6833369e6
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-07 13:39:26 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,6 +7,8 @@ class RepositoriesController < ApplicationController
     begin
       @resp = Faraday.get "https://api.github.com/search/repositories?q=#{params[:query]}"
       binding.pry
+      body = JSON.parse(@resp.body)
+      @resp.success? ? (@venues = body["response"]["venues"]) : (@error = body["meta"]["errorDetail"])
     rescue Faraday::ConnectionFailed
       @error = "Timeout error. Please try again."
     end
